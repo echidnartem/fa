07b05854eebe4fc032b9f618c41342e98d2549c7
Artyom Mitskevich <artemkurskzoloto@mail.ru>
2025-09-25 23:39:13 +0300

    Lab

---
+++ 1.1/include/flags.h
@@ -0,0 +1,9 @@
+#include <stdbool.h>
+
+typedef long long ll;
+bool h(int * result, const ll * number);
+bool p(const ll * number);
+int s(char * result, const ll * number);
+void e(ll * result, const ll * number);
+ll a(const ll * number);
+ll f(const ll * number);
